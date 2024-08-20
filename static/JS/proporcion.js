c1100f8f86ea0753c2d588dd64dd07e8d3a80b29
Manu <manuelvm324@gmail.com>
2024-08-20 11:14:40 -0700

    Seccion gorras

---
+++ static/JS/proporcion.js
@@ -0,0 +1,22 @@
+
+// let header = document.querySelector("header")
+// let section = document.querySelector("section")
+// let nav = document.querySelector("nav")
+// let ul = document.querySelector("ul")
+// let body = document.querySelector("body")
+
+// window.onresize = function () {
+//     header.setAttribute("width", `${screen.width}`)
+//     section.setAttribute("width", `${screen.width}`)
+//     nav.setAttribute("width", `${screen.width}`)
+//     ul.setAttribute("width", `${screen.width}`)
+// }
+
+let button = document.getElementById("btn")
+
+
+function proporcion () {
+    alert(`ancho: alto: `)
+}
+
+button.addEventListener("click", proporcion)
