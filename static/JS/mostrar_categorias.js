8571ca7e17ba1ccb9b5feb2ec6ecee46a058a4a0
Manu <manuelvm324@gmail.com>
2024-08-23 10:59:51 -0700

    Columna filtros actualizada / Alineacion de tarjetas

--- static/JS/mostrar_categorias.js
+++ static/JS/mostrar_categorias.js
@@ -1,9 +1,19 @@
 
-function alerta () {
-    const linea = document.getElementById("vertical_line");
-    linea.style.transform = rotate(90)
+const boxes = document.getElementsByClassName("box_btn")
+const options = document.getElementsByClassName("options")
+let click = [false, false, false, false]
+
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", function () {
+        const lineas = document.getElementsByClassName("vertical_line")
+        if (click[i] == false) {
+            lineas[i].style.transform = 'rotate(-90deg)'
+            options[i].style.height = '150px'
+            click[i] = true
+        } else {
+            lineas[i].style.transform = 'rotate(0deg)'
+            options[i].style.height = '0px'
+            click[i] = false
+        }
+    })
 }
-
-const box = document.getElementById("box_btn");
-
-box.addEventListener("click", alerta);
