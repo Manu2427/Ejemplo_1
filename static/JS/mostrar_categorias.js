7e79f41d71c9f9d9cbc862947bdad1c993566cac
Manu <manuelvm324@gmail.com>
2024-08-27 19:22:07 -0700

    Correcion de seccion de filtros

--- static/JS/mostrar_categorias.js
+++ static/JS/mostrar_categorias.js
@@ -3,17 +3,36 @@ const boxes = document.getElementsByClassName("box_btn")
 const options = document.getElementsByClassName("options")
 let click = [false, false, false, false]
 
+function alerta (e) {
+    let target = e.target
+    let lineaVertical = target.querySelector(".linea_vertical")
+    let parent = target.parentElement.parentElement
+    let options = parent.querySelector(".options")
+    options.style.height = '200px'
+    parent.style.backgroundColor = 'red'
+    target.style.backgroundColor = 'red'
+    lineaVertical.style.transform = 'rotate(-90deg)'
+}
+
 for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener("click", function () {
-        const lineas = document.getElementsByClassName("vertical_line")
-        if (click[i] == false) {
-            lineas[i].style.transform = 'rotate(-90deg)'
-            options[i].style.height = '150px'
-            click[i] = true
-        } else {
-            lineas[i].style.transform = 'rotate(0deg)'
-            options[i].style.height = '0px'
-            click[i] = false
-        }
-    })
+    boxes[i].addEventListener("click", alerta)
+        // function (e) {
+        // const lineas = document.getElementsByClassName("vertical_line")
+        // if (click[i] == false) {
+        //     lineas[i].style.transform = 'rotate(-90deg)'
+        //     options[i].style.height = '150px'
+        //     // let child = options[i].children
+        //     // for (element in child) {
+        //     //     element.style.opacity = '0'
+        //     // }
+        //     click[i] = true
+        // } else {
+        //     lineas[i].style.transform = 'rotate(0deg)'
+        //     options[i].style.height = '0px'
+        //     // for (element in child) {
+        //     //     element.style.opacity = '1'
+        //     // }
+        //     click[i] = false
+        // }
+//    })
 }
